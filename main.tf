7080badecaed4084255e118e14c3a99aee78c141
Belendor <61137829+Belendor@users.noreply.github.com>
2022-12-19 16:01:33 +0200

    for each

--- main.tf
+++ main.tf
@@ -22,27 +22,33 @@ provider "dominos" {
 }
 
 data "dominos_address" "addr" {
-  street      = var.address["street"]
-  city        = var.address["city"]
-  state       = var.address["state"]
-  zip         = var.address["zip"]
+  street = var.address["street"]
+  city   = var.address["city"]
+  state  = var.address["state"]
+  zip    = var.address["zip"]
 }
 
 output "AddressData" {
   value = data.dominos_address.addr
 }
 
-# data "dominos_store" "store" {
-#   address_url_object = "{\"line1\":\"6199 NE Alber st\",\"line2\":\"Hillsboro OR 97124\"}" //data.dominos_address.addr.url_object
-# }
+data "dominos_store" "store" {
+  address_url_object = data.dominos_address.addr.url_object
+}
 
-# data "dominos_menu_item" "item" {
-#   store_id     = 7218
-#   query_string = ["philly", "medium"]
-# }
+data "dominos_menu_item" "item" {
+  store_id     = data.dominos_store.store.store_id
+  query_string = ["philly", "medium"]
+}
 
-# resource "dominos_order" "order" {
-#   address_api_object = data.dominos_address.addr.api_object
-#   item_codes         = ["${data.dominos_menu_item.item.matches.0.code}"]
-#   store_id           = data.dominos_store.store.store_id
-# }
+resource "dominos_order" "order" {
+
+  for_each = {
+    0 = data.dominos_menu_item.item.matches.0.code
+    1 = data.dominos_menu_item.item.matches.1.code
+  }
+
+  address_api_object = data.dominos_address.addr.api_object
+  item_codes         = [each.value]
+  store_id           = data.dominos_store.store.store_id
+}
